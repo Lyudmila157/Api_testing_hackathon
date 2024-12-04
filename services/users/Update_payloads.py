0bc31165d2e9c695168c94d60c05f6ecb0a008c7
Lyudmila157 <145120355+Lyudmila157@users.noreply.github.com>
2024-12-04 07:14:36 +0000

    Deploying to gh-pages from @ Lyudmila157/Api_testing_hackathon@c99c6b3077ad8f0e390642d5ef7bf43450c30b25 🚀

--- services/users/Update_payloads.py
+++
@@ -1,9 +0,0 @@
-
-class UpdatePayloads:
-    update_user = {
-        "email": "max@gmail.com",
-        "name": "Max",
-        "nickname": "max",
-        "avatar_url": "",
-        "uuid": "00000000-0000-4562-b3fc-2c963f66afa6"
-    }
--- services/users/params.py
+++
@@ -1,3 +0,0 @@
-
-class Params:
-    params = {"offset": 10}
